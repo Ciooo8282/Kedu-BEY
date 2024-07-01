36ca513bd7bdcbeb4b703fd10066dec8be9ceac5
Ciooo8282 <cionecokemal4434@gmail.com>
2024-07-01 10:15:26 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,20 +2,20 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <h1 className="font-semibold text-3xl text-white/75">MusicMaker için Gizlilik Politikası</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 29/062024  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Oy vermek zorunda değilsin yakında gelicektir <a className="text-white" href=""Bot komutlarından belirli komutları kullanırken >MusicMaker</a> bot.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı, zarar vermek istemedikleri sürece Keduu botunu istediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <p>⊳ Keduuu kullanırken belirli komutları aynı anda kullanamazsınız.</p> 
         <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
+        <p>⊳ Keduuu botu, kullanıcılara dayalı olarak herhangi bir veri kaydetmez.</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Botu kullanırken, kullanıcı kimliğiniz bir kez olsun 60 saniye boyunca sistemde kaydedilir ve silinir..</p> 
     
         </div>
             </div>
